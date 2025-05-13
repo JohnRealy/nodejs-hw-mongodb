7cec43a51f3da091c2a7a0d9cf323eece96a5e05
JohnRealy <branderok@gmail.com>
2025-05-13 16:38:08 +0200

    bug fixed

--- src/server.js
+++ src/server.js
@@ -7,8 +7,20 @@ import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import router from './routers/index.js';
 import cookieParser from 'cookie-parser';
 import { UPLOAD_DIR } from './constants/index.js';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import path from 'node:path';
+import fs from 'node:fs';
+import swaggerUi from 'swagger-ui-express';
 
 const PORT = Number(env('PORT', '3000'));
+
+const filename = fileURLToPath(import.meta.url);
+const dirpath = dirname(filename);
+
+const swaggerPath = path.join(dirpath, '..', 'docs', 'swagger.json');
+
+const swaggerDocument = JSON.parse(fs.readFileSync(swaggerPath, 'utf-8'));
 
 export const setupServer = () => {
   const app = express();
@@ -17,14 +29,18 @@ export const setupServer = () => {
   app.use(cors());
   app.use(cookieParser());
 
-  app.use(
-    pino({
-      transport: {
-        target: 'pino-pretty',
-      },
-    }),
-  );
+  // app.use(
+  //   pino({
+  //     transport: {
+  //       target: 'pino-pretty',
+  //     },
+  //   }),
+  // );
+
   app.use('/uploads', express.static(UPLOAD_DIR));
+
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
   app.use(router);
 
   app.use('*', notFoundHandler);
